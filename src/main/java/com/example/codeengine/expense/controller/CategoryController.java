19070bd50d0f50ab081ed4c62d9b2e21ba36f4f8
Miles Higbie <miles.higbie@gmail.com>
2021-04-19 11:56:39 -0400

    cors

--- src/main/java/com/example/codeengine/expense/controller/CategoryController.java
+++ src/main/java/com/example/codeengine/expense/controller/CategoryController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.codeengine.expense.model.Category;
 import com.example.codeengine.expense.repository.CategoryRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
